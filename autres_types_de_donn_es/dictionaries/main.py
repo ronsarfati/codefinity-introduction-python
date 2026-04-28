eeed114d9b9f9085fa1a99542898e2eb50c50c3d
ronsarfati <>
2026-04-28 12:47:40 +0000

    Dictionaries

---
+++ autres_types_de_donn_es/dictionaries/main.py
@@ -0,0 +1,18 @@
+grocery_inventory = {
+    "Milk" : (113, "Dairy"),
+    "Eggs" : (116, "Dairy"),
+    "Bread" : (117, "Bakery"),
+    "Apples" : (141, "Produce")
+}
+
+bread_details = grocery_inventory.get("Bread")
+#print(f"Details of Breads: {bread_details}")
+
+grocery_inventory.update({"Cookies" : (143, "Bakery")})
+print(f"Inventory after adding Cookies: {grocery_inventory}")
+
+grocery_inventory.pop("Eggs")
+print(f"Inventory after removing Eggs: {grocery_inventory}")
+
+print(f"Details of Bread: {bread_details}")
+
