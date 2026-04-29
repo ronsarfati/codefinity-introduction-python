b1057d4d62f1993a7775b9f6679846a8a4b44633
ronsarfati <>
2026-04-29 11:21:24 +0000

    Défi : Fonctions Zip et Tri

---
+++ fonctions/d_fi_fonctions_zip_et_tri/main.py
@@ -0,0 +1,17 @@
+# List of product names
+products = ["Banana", "Apple", "Mango", "Cherry"]
+
+# List of product prices
+prices = [1.20, 0.50, 2.50, 1.75]
+
+# List of quantity sold
+quantities_sold = [50, 100, 25, 40]
+
+combined_list = list(zip(products, prices, quantities_sold))
+#print(combined_list)
+
+sorted_products = sorted(combined_list)
+
+for product, price, quantity_sold in sorted_products:
+    print(f"Product: {product}, Price: {price}, Quantity Sold: {quantity_sold}")
+    
